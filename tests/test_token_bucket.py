346a48ded690b4479d0cbeacbd7376e26829b11c
Bart K <bartlomiej.kornowski@gmail.com>
2025-02-15 15:46:07 +0100

    Added unit test coverage for token_bucket.py

--- tests/test_token_bucket.py
+++ tests/test_token_bucket.py
@@ -1,21 +1,49 @@
-import pytest
+import time
 from token_bucket import TokenBucket
-from unittest.mock import patch, MagicMock
+from multiprocessing import Manager
 
 
-@patch("multiprocessing.Manager")
-def test_init(mock_manager):
-    mock_manager_instance = MagicMock()
-    mock_manager_instance.list.return_value = []
-    mock_manager.return_value = mock_manager_instance
+def test_init():
 
     token_bucket = TokenBucket(
         bucket_size=5,
         refill_amount=1,
         refill_interval=1,
-        manager=mock_manager
+        manager= Manager()
     )
     assert token_bucket.bucket_size == 5
     assert token_bucket.refill_amount == 1
     assert token_bucket.refill_interval == 1
+    assert len(token_bucket.tokens) == 5
 
+def test_get_token():
+
+    token_bucket = TokenBucket(
+        bucket_size=5,
+        refill_amount=1,
+        refill_interval=1,
+        manager=Manager()
+    )
+
+    token = token_bucket.get_token("test request")
+    assert token == "*"
+
+
+def test_refill():
+
+    token_bucket = TokenBucket(
+        bucket_size=5,
+        refill_amount=1,
+        refill_interval=1,
+        manager=Manager()
+    )
+
+    token = token_bucket.get_token("test request")
+    token = token_bucket.get_token("test request")
+    token = token_bucket.get_token("test request")
+
+    time.sleep(1)
+
+    token_bucket.refill("test process")
+
+    assert len(token_bucket.tokens) == 3
